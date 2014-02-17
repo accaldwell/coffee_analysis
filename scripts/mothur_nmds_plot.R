fd4020516a3975a6bcceb0b83ab3568abceb0c5c
Adam Caldwell <adam.caldwell@gmail.com>
2014-02-17 13:15:27 -0800

    Misc fixes and additions

--- scripts/mothur_nmds_plot.R
+++ scripts/mothur_nmds_plot.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 
-axes <- read.table("/data/argonne2/manuscript-arg2_COF_motan8_ss3k/abund.subsample.filter.an.unique_list.wcount.thetayc.0.03.lt.nmds.axes", header=T)
+axes <- read.table("abund.subsample.filter.an.unique_list.wcount.thetayc.0.03.lt.nmds.axes", header=T)
 
-COF_meta <- read.delim("/data/argonne2/manuscript-arg2_COF_motan8_ss3k/COF_meta_full.csv")
+COF_meta <- read.delim("COF_meta_full.csv")
 
 axes_wMeta <- merge(axes, COF_meta, by="group")
 
@@ -10,8 +10,8 @@ axes_wMeta <- merge(axes, COF_meta, by="group")
 
 #ggplot(axes_wMeta, aes(axis1, axis2, colour=Site2, label=group)) + geom_point(size=3) + ggtitle("PCoA, by Sample Type") + geom_text(hjust=0, vjust=0) + theme(legend.position="bottom")
 
-pdf("COF_NMDS_SiteSampleType.pdf", width=12, height=10)
+pdf("Figure2_NMDS_SiteSampleType.pdf", width=12, height=10)
 
-ggplot(axes_wMeta, aes(axis1, axis2, colour=Site, shape=SampleType)) + geom_point(size=3) + ggtitle("NMDS, by Site + Sample Type (R-squared 0.695)") + theme(legend.position="bottom")
+ggplot(axes_wMeta, aes(axis1, axis2, colour=Site, shape=SampleType)) + geom_point(size=3) + theme(legend.position="bottom")
 
 dev.off()
